dbacd3e2da863cc491b0cb2c6576fdf1eeee8916
Kritagyabhagat <117289078+Kritagyabhagat@users.noreply.github.com>
2023-09-05 23:07:48 +0530

    Create Addition.c

---
+++ Addition.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+  int a,b;
+  printf("Enter first number : ");
+  scanf("%d",&a);
+  printf("Enter second number : ");
+  scanf("%d",&b);
+  printf("%d + %d : %d",a,b,a+b);
+  return 0;
+}
