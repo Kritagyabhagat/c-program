b4a22e05b74086cd9691eb1a7981a4c313bb00d0
Kritagyabhagat <117289078+Kritagyabhagat@users.noreply.github.com>
2023-09-05 23:19:27 +0530

    Update and rename Addition.c to Mathematical operations.c

---
+++ Mathematical operations.c
@@ -0,0 +1,55 @@
+//  Adition
+
+#include <stdio.h>
+int main()
+{
+  int a,b;
+  printf("Enter first number : ");
+  scanf("%d",&a);
+  printf("Enter second number : ");
+  scanf("%d",&b);
+  printf("%d + %d : %d \n",a,b,a+b);
+  return 0;
+}
+
+//  Subtraction
+
+#include <stdio.h>
+int main()
+{
+  int a,b;
+  printf("Enter first number : ");
+  scanf("%d",&a);
+  printf("Enter second number : ");
+  scanf("%d",&b);
+  printf("%d - %d : %d \n",a,b,a-b);
+  return 0;
+}
+
+//  Multiplaction
+
+#include <stdio.h>
+int main()
+{
+  int a,b;
+  printf("Enter first number : ");
+  scanf("%d",&a);
+  printf("Enter second number : ");
+  scanf("%d",&b);
+  printf("%d * %d : %d \n",a,b,a*b);
+  return 0;
+}
+
+//  Division
+
+#include <stdio.h>
+int main()
+{
+  int a,b;
+  printf("Enter first number : ");
+  scanf("%d",&a);
+  printf("Enter second number : ");
+  scanf("%d",&b);
+  printf("%d / %d : %d \n",a,b,a/b);
+  return 0;
+}
